c54b2db0fff21a351aa6dd458da9dffd31e37954
Sean Harrison <sah@kruxia.com>
2025-11-27 20:04:54 -0600

    US-5.7a email_send implemented

--- worker/src/activities/mod.rs
+++ worker/src/activities/mod.rs
@@ -1,9 +1,11 @@
 mod echo;
+pub mod email;
 mod http;
 mod llm;
 mod postgres;
 
 pub use echo::EchoActivity;
+pub use email::EmailSendActivity;
 pub use http::HttpRequestActivity;
 pub use llm::{EmbeddingActivity, LLMPromptActivity};
 pub use postgres::{PostgresQueryActivity, PostgresTransactionActivity, new_pool_cache};
